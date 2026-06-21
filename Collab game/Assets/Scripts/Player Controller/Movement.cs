e8b3cd70192d13c64b8bc94c4d8da3214a37a67c
Stidiot <angelvazromo25@gmail.com>
2026-06-21 14:42:07 -0500

    Finished basic movement and added jumping
    Ok

--- Collab game/Assets/Scripts/Player Controller/Movement.cs
+++ Collab game/Assets/Scripts/Player Controller/Movement.cs
@@ -2,7 +2,23 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    [Header("Movement")]
     public float moveSpeed;
+    public float groundDrag;
+
+    public float jumpForce;
+    public float jumpCD;
+    public float airMultiplier;
+    bool readyToJump = true;
+
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space; //Add keybind options later maybe
+
+    [Header("Ground Check")]
+    public LayerMask whatIsGround;
+    public Transform groundCheck;
+    public float groundDistance;
+    bool grounded;
     public Transform orientation;
 
     float horizontalInput;
@@ -19,7 +35,18 @@ public class Movement : MonoBehaviour
 
     private void Update()
     {
+        //Ground check
+        grounded = Physics.CheckSphere(groundCheck.position, groundDistance, whatIsGround); 
+
+
         myInput();
+        speedControl();
+
+        //Drag
+        if (grounded)
+            rigidBody.linearDamping = groundDrag;
+        else
+            rigidBody.linearDamping = groundDrag * 0.05f;
     }
 
     private void FixedUpdate()
@@ -31,12 +58,47 @@ public class Movement : MonoBehaviour
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(jumpKey) && readyToJump && grounded)
+        {
+            readyToJump = false;
+            jump();
+            Invoke(nameof(resetJump), jumpCD);
+        }
     }
 
     private void movePlayer()
     {
         //Calculate movement direction
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
-        rigidBody.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+
+        if(grounded) //Normal speed on ground that guy has
+            rigidBody.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);  
+        else if(!grounded) // Guy is in air
+            rigidBody.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);  
+    }
+
+    private void speedControl()
+    {
+        Vector3 flatVelocity = new Vector3(rigidBody.linearVelocity.x, 0f, rigidBody.linearVelocity.z);
+
+        // If guy is going faster than set movespeed then execute them brutally
+
+        if (flatVelocity.magnitude > moveSpeed)
+        {
+            Vector3 limitedVelocity = flatVelocity.normalized * moveSpeed;
+            rigidBody.linearVelocity = new Vector3(limitedVelocity.x, rigidBody.linearVelocity.y, limitedVelocity.z);
+        }
+    }
+
+    private void jump()
+    {
+        //Reset y velocity first
+        rigidBody.linearVelocity = new Vector3(rigidBody.linearVelocity.x, 0f, rigidBody.linearVelocity.z);
+        rigidBody.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+    private void resetJump()
+    {
+        readyToJump = true;
     }
 }
