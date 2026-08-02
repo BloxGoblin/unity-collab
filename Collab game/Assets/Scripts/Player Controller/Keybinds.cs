018b570e6636890e1658ed78dcf333c101029669
Stidiot <angelvazromo25@gmail.com>
2026-08-02 17:25:51 -0500

    I dont know man

---
+++ Collab game/Assets/Scripts/Player Controller/Keybinds.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Keybinds : MonoBehaviour
+{
+    [Header("Gameplay")]
+    public KeyCode interact = KeyCode.E;
+    public KeyCode openInventory = KeyCode.Tab;
+}
